0a0a58f034aa2f0acef455d87dfe8cf031049c15
kylegeorge10 <kjg5690@psu.edu>
2021-03-29 23:04:29 -0400

    Initial Commit

---
+++ myFridge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myFridge
+//
+//  Created by Kyle George on 3/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
