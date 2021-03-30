d64b8adc1377d1b311e7c709266dd073edbc2aea
kylegeorge10 <kjg5690@psu.edu>
2021-03-30 15:46:52 -0400

    Started working on the Home feed with the details view controller added as well as removed the labels and kept the images on the tab bar navigation controller

---
+++ myFridge/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  myFridge
+//
+//  Created by Kyle George on 3/30/21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var backgroundRecipeImage: UIImageView!
+    @IBOutlet weak var foodImage: UIImageView!
+    @IBOutlet weak var recipeNameLabel: UILabel!
+    @IBOutlet weak var reviewsLabel: UILabel!
+    @IBOutlet weak var fullRecipeLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ myFridge/HomeViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewCell.swift
+//  myFridge
+//
+//  Created by Kyle George on 3/30/21.
+//
+
+import UIKit
+
+class HomeViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageFood: UIImageView!
+    @IBOutlet weak var recipeNameLabel: UILabel!
+    @IBOutlet weak var reviewsLabel: UILabel!
+    @IBOutlet weak var recipeSummaryLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
