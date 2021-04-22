974154dacbdd495539e10811dae873eaaa17cfc9
Senuda Ratnayake <tdr5250@psu.edu>
2021-04-22 14:27:11 +0530

    Worked on discovery Page obtaining data and storing in a list for later use

--- myFridge/ViewControllers/DiscoveryGridViewController.swift
+++ myFridge/ViewControllers/DiscoveryGridViewController.swift
@@ -7,20 +7,60 @@
 
 import UIKit
 
-class DiscoveryGridViewController: UIViewController {
+class DiscoveryGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
 
     @IBOutlet weak var addItemsTextField: UITextField!
+    @IBOutlet weak var showItemsLabel: UILabel!
+    @IBOutlet weak var foundPostImageView: UIImageView!
+    @IBOutlet weak var foundPostRecipeNameLabel: UILabel!
+    
+    // declare a list to store the user's fridge inventory
+    var itemList: Array<String> = Array()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func onNextItemButton(_ sender: Any) {
+        itemList.append(addItemsTextField.text!)
+        if itemList != nil{
+            print(itemList)
+            showItemsLabel.text = itemList.joined(separator: ", ")
+        } else {
+            print("None")
+        }
+    }
+    
+    @IBAction func onClearButton(_ sender: UIButton) {
+        if itemList != []{
+            itemList.popLast()
+            showItemsLabel.text = itemList.joined(separator: ", ")
+        } else {
+            showItemsLabel.text = "List of Items I have"
+        }
+    }
+    
+    
+    
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         //Dismiss the keyboard when not in use
         
         addItemsTextField.resignFirstResponder()
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
 
     /*
     // MARK: - Navigation
--- myFridge/ViewControllers/ProfileViewController.swift
+++ myFridge/ViewControllers/ProfileViewController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 import Parse
+import AlamofireImage
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
     
     @IBOutlet weak var usernameLabel: UILabel!
         
@@ -28,6 +31,49 @@ class ProfileViewController: UIViewController {
         
         
       }
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            picker.sourceType = .camera
+        }else{
+            picker.sourceType = .photoLibrary
+        }
+        present(picker, animated: true, completion: nil)
+    }
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageAspectScaled(toFill: size)
+        
+        profileImageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
+        
+        let post = PFObject(className: "Posts")
+        
+        let profileImageData = profileImageView.image!.pngData()
+        let file = PFFileObject(name: "profileImage.png", data: profileImageData!)
+        
+        post["profileImage"] = file
+        
+        post.saveInBackground { (success, error) in
+            if success{
+                self.dismiss(animated: true, completion: nil)
+                print("saved!")
+            }
+        }
+    }
+        
+    
+    
     
 
     /*
