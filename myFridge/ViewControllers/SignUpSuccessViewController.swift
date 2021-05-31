66f49f3557ac6751ed90acab5ae092521e816683
Senuda Ratnayake <tdr5250@psu.edu>
2021-05-31 17:07:53 +0530

    changed the signing transition view controller

---
+++ myFridge/ViewControllers/SignUpSuccessViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpSuccessViewController.swift
+//  myFridge
+//
+//  Created by Senuda Ratnayake on 5/31/21.
+//
+
+import UIKit
+import Lottie
+
+class SignUpSuccessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
